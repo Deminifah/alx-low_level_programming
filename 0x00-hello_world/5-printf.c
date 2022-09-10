d0d22c46e4abd2b74d8150344d831caff76896c9
Deminifah <nkanujackson5@gmail.com>
2022-09-10 06:57:59 -0700

    Deminifah

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include<unistd.h>
-
 /**
- * main - the main function
- * Return: returns 1
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

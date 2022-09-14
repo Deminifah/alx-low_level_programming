7641c4cd97e80f8d66f0f82cb404fc6de7f39203
Deminifah <nkanujackson5@gmail.com>
2022-09-14 11:46:39 -0700

    Deminifah

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-15-19\n"");
+	int 89 = strlen("and that piece of art is useful\n" - Dora Korpar, 2015-15-19);
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19);
 	return (1);
 }

0f1677fd684ef3375ff3944bffeba52b9b32d912
Deminifah <nkanujackson5@gmail.com>
2022-09-23 02:57:59 -0700

    Deminifah

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Description: Write a function that concatenates two strings
+ * Return: Always 0 (success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; scr[index]; index++)
+		dest[dest_len++] = scr[index];
+	return (dest)
+
+}
